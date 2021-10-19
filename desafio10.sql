8ee9dd8cf471bcfd842d26616b0d0922d7936ac9
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-19 17:05:43 -0300

    corrige o requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,4 +2,5 @@
 
 SELECT * FROM northwind.purchase_orders
 WHERE created_by >= 3
-ORDER BY id, created_by DESC;
+-- A ordernação pelo created_by deve ocorrer antes de ordernar pelo id
+ORDER BY created_by DESC, id;
