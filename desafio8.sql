7be03c4648cf1f376e982985b64651e8a03e8d74
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-19 16:15:08 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,3 @@
--- Desafio 8
+-- 8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
+
+SELECT 5 + 6 AS 'A', 'de' AS 'Trybe', 2 + 8 AS 'eh';
