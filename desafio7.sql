d163a92f211f35c398bdf5a0fd352437404ee204
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-19 17:23:38 -0300

    corrige typo requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,4 @@ SELECT * FROM northwind.products
 -- Para mostrar os 5 últimos registros a ordem da tabela deve ser invertida
 -- É possivel fazer isso ordenando a tabela pela primary_key em ordem descendente
 ORDER BY id DESC
-DESC LIMIT 5;
+LIMIT 5;
