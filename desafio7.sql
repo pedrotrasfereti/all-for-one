58ffba6ac0494d35dad7cd583bd83e3aa1b7c1c4
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-19 17:03:59 -0300

    corrige o requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,7 @@
 -- 7 - Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
 
-SELECT * FROM northwind.products ORDER BY id DESC LIMIT 5;
-
+SELECT * FROM northwind.products
+-- Para mostrar os 5 últimos registros a ordem da tabela deve ser invertida
+-- É possivel fazer isso ordenando a tabela pela primary_key em ordem descendente
+ORDER BY id DESC
+DESC LIMIT 5;
